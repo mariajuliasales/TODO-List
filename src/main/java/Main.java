b0824a6ba597407791d0c2505dde34784431515c
mariajuliasales <salesmariajulia11@gmail.com>
2026-08-20 10:25:51 -0300

    feat: implementa método para cadastrar tarefa

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,15 @@
 import service.TarefaService;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Main {
 
     private static final TarefaService service = new TarefaService();
     private static final Scanner scanner = new Scanner(System.in);
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
 
     public static void main(String[] args) {
 
@@ -16,7 +20,7 @@ public class Main {
 
             opc = Integer.parseInt(scanner.nextLine());
             switch (opc) {
-                case 1 -> System.out.println("implementar cadastro");
+                case 1 -> cadastrarTarefa();
                 case 2 -> System.out.println("implementar listagem");
                 case 3 -> System.out.println("implementar atualização");
                 case 4 -> System.out.println("implementar deleção");
@@ -35,5 +39,21 @@ public class Main {
         System.out.println("4. Deletar Tarefa");
         System.out.println("0. Sair do programa");
         System.out.print("Escolha uma opção: ");
+    }
+
+    private static void cadastrarTarefa() {
+        System.out.print("Nome: ");
+        String nome = scanner.nextLine();
+        System.out.print("Descrição: ");
+        String descricao = scanner.nextLine();
+        System.out.print("Data Término (dd/MM/yyyy): ");
+        LocalDate dataTermino = LocalDate.parse(scanner.nextLine(), formatter);
+        System.out.print("Categoria: ");
+        String categoria = scanner.nextLine();
+        System.out.print("Prioridade (1 a 5): ");
+        int prioridade = Integer.parseInt(scanner.nextLine());
+
+        service.adicionar(nome, descricao, dataTermino, prioridade, categoria);
+        System.out.println("Tarefa cadastrada com sucesso!");
     }
 }
