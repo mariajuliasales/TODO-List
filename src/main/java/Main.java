927a2856f95890e4af36c7f23cf7df1d6873514e
mariajuliasales <salesmariajulia11@gmail.com>
2026-08-20 14:42:27 -0300

    feat: implementa métodos de listagem com filtros por cateogria, prioridade e status

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
+import model.StatusEnum;
+import model.Tarefa;
 import service.TarefaService;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -21,7 +24,7 @@ public class Main {
             opc = Integer.parseInt(scanner.nextLine());
             switch (opc) {
                 case 1 -> cadastrarTarefa();
-                case 2 -> System.out.println("implementar listagem");
+                case 2 -> listarTarefas();
                 case 3 -> System.out.println("implementar atualização");
                 case 4 -> System.out.println("implementar deleção");
                 case 0 -> System.out.println("Saindo do sistema...");
@@ -55,5 +58,46 @@ public class Main {
 
         service.adicionar(nome, descricao, dataTermino, prioridade, categoria);
         System.out.println("Tarefa cadastrada com sucesso!");
+    }
+
+    private static void listarTarefas(){
+        System.out.println("\n--- LISTAR TAREFAS ---");
+        System.out.println("1. Listar todas as tarefas");
+        System.out.println("2. Listar por categoria");
+        System.out.println("3. Listar por prioridade");
+        System.out.println("4. Listar por status");
+        System.out.print("Escolha uma opção: ");
+        int opcao = Integer.parseInt(scanner.nextLine());
+
+        switch (opcao){
+            case 1 -> { List<Tarefa> tarefas = service.listar();
+                if (tarefas.isEmpty()) {
+                    System.out.println("Nenhuma tarefa cadastrada!");
+                } else {
+                    System.out.println("\n--- LISTA DE TAREFAS ---");
+                    tarefas.forEach(t -> System.out.println(t));
+                }
+            }
+            case 2 -> {
+                System.out.print("Digite a categoria: ");
+                String categoria = scanner.nextLine();
+                System.out.println("Listando tarefas da categoria: " + categoria);
+                service.listarPorCategoria(categoria).forEach(t -> System.out.println(t));
+            }
+            case 3 -> {
+                System.out.print("Digite a prioridade (1 a 5): ");
+                int prioridade = Integer.parseInt(scanner.nextLine());
+                System.out.println("Listando tarefas com prioridade: " + prioridade);
+                service.listarPorPrioridade(prioridade).forEach(t -> System.out.println(t));
+            }
+            case 4 -> {
+                System.out.print("Digite o status (TODO, DOING, DONE): ");
+                StatusEnum status = StatusEnum.valueOf(scanner.nextLine());
+                System.out.println("Listando tarefas com status: " + status);
+                service.listarPorStatus(status).forEach(t -> System.out.println(t));
+            }
+            default -> System.out.println("Opção inválida!");
+        }
+
     }
 }
