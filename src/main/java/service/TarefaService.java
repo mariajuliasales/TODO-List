cc83eb264cd3f3f4e539cbf3dade1bbaee3fe955
mariajuliasales <salesmariajulia11@gmail.com>
2026-08-20 09:51:07 -0300

    feat: adiciona TarefaService com método para criar nova tarefa

---
+++ src/main/java/service/TarefaService.java
@@ -0,0 +1,24 @@
+package service;
+
+import model.Tarefa;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+public class TarefaService {
+
+    private List<Tarefa> tarefas = new ArrayList<>();
+    private final AtomicInteger contadorId = new AtomicInteger(0); // Contador inicia em 0
+
+    public Tarefa adicionar(String nome, String descricao, LocalDate dataTermino, int prioridade, String categoria) {
+        int novoId = contadorId.getAndIncrement();
+        Tarefa novaTarefa = new Tarefa(novoId, nome, descricao, dataTermino, prioridade, categoria);
+        tarefas.add(novaTarefa);
+        return novaTarefa;
+    }
+
+}
